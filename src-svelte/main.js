cb6d8a9149e514c69f79958d10f953f605cae844
Josef Johansson <josef@threefs.net>
2023-04-22 01:13:46 +0200

    moved svelte files

---
+++ src-svelte/main.js
@@ -0,0 +1,11 @@
+import App from './App.svelte';
+import './css/minireset.min.css';
+import './css/bulma-custom.css';
+
+const app = new App({
+	target: document.body,
+	props: {
+	}
+});
+
+export default app;
--- src/main.js
+++ src/main.js
@@ -1,11 +1,1 @@
-import App from './App.svelte';
-import './css/minireset.min.css';
-import './css/bulma-custom.css';
-
-const app = new App({
-	target: document.body,
-	props: {
-	}
-});
-
-export default app;
+https://github.com/11ty/webc
